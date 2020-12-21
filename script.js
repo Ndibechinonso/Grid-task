977b12af033b90864985a03cd8b5d84dc4f0e118
Ndibechinonso <edwardndibe@gmail.com>
2020-12-21 11:19:15 +0100

    updated edits

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let pics = Array.prototype.slice.call(pic);
 let random = Math.floor(Math.random() * pics.length + 1);
 
 function picsDisplay() {
-for ( let i = o; i < random.lenght; i++) {
+for ( let i = 0; i < random.lenght; i++) {
 pic.innerHTML = random;
 } }
 picsDisplay()
